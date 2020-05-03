638e6375ee9879c0b530cd0ebc262c529bdd2ed5
Kerim Guseynov <guseynovkerim@gmail.com>
2020-05-03 21:38:36 +0300

    Moved dwm files

--- to_home/dwm/autostart.sh
+++ to_home/dwm/autostart.sh
@@ -1,7 +1,7 @@
 xrdb -load ~/.Xresources
 feh --bg-fill ~/.wallpapers/foggy-forest-light.jpg #bliss.jpg #dusk-fog.jpg  #macOS.jpg #water-rails.jpg
 ~/.bin/kb
-~/.dwm/status/status &
+~/.status/dwms &
 clipmenud &
 dunst &
 ~/.bin/compton &
